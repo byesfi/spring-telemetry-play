67f88cc063195a844dad5eb59236295296cea132
Bilal Yesfi <yesfi.bilal@gmail.com>
2024-01-13 22:51:31 +0100

    feat: initialize spring-telemetry-play project

---
+++ src/main/java/com/byesfi/springtelemetryplay/SpringTelemetryPlayApplication.java
@@ -0,0 +1,13 @@
+package com.byesfi.springtelemetryplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTelemetryPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTelemetryPlayApplication.class, args);
+	}
+
+}
